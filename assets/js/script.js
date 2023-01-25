41b26a7eb82345be736f8958ce76eecc28a7352a
SolForte <mateusbrandao1234@gmail.com>
2023-01-25 05:07:02 -0300

    Com randomização de alternativas

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,12 +21,41 @@ function getChoosenQuizzData(selection){
 };
 function renderChoosenQuizz(quizzInfo){
   const quizzes = document.querySelector(".allQuizzes");
-  console.log(quizzInfo)
+  //Aqui embaixo é inserido o banner que fica no topo do Quizz a imagem e o título do quizz.
   quizzes.innerHTML=`
-  <li>
-    <img src="${quizzInfo.data.image}">
-    <p>${quizzInfo.data.title}</p>
-  </li>
+  <div>
+        <img src="${quizzInfo.data.image}">
+        <p>${quizzInfo.data.title}</p>
+  </div>
   `
+  //Aqui embaixo é inserido cada questão individual do Quizz
+  //No caso, é uma div que tem o título da questão, e outra div que dentro dela contem as alternativas para esta questão
+  for (let i=0; i<quizzInfo.data.questions.length;i++){
+    quizzes.innerHTML+=`
+    <div>
+      <p>${quizzInfo.data.questions[i].title}</p>
+      <div class="options-of-question-${i}"></div>
+    </div>`
+  //Atenção aqui em cima! Pode inserir classes adicionais para formatar como desejar no CSS,
+  //mas cuidado se for mexer nas classes já existentes
+  }
+  for (let i=0; i<quizzInfo.data.questions.length;i++){
+    //Se não colocasse esse for aqui em cima, o query selector abaixo sempre escolheria a primeira
+    //div que aparece. Mas com o for, é possível percorrer todas as divs de questões.
+    const quizzOptions = document.querySelector(`.options-of-question-${i}`)
+    quizzOptions.innerHTML=""
+    for (let j=0; j<quizzInfo.data.questions[i].answers.length;j++){
+      quizzInfo.data.questions[i].answers.sort(shuffle);
+      quizzOptions.innerHTML+=`
+      <div>
+        <img src="${quizzInfo.data.questions[i].answers[j].image}" alt="">
+        <p>${quizzInfo.data.questions[i].answers[j].text}</p>
+      </div>
+      `
+    }
+  }
+}
+function shuffle() {
+  return Math.random() - 0.5;
 }
 getQuizzes();

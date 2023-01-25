ef36da688b09cf4432b6153c782c3e81e7ff631a
SolForte <mateusbrandao1234@gmail.com>
2023-01-25 02:10:58 -0300

    Fatoração da função de obter o quizz selecionado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,32 @@
-let globalQuizzInfo = undefined;
 function getQuizzes() {
   const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
   promise.then(renderAllQuizzes);
-}
+};
 function renderAllQuizzes(quizzInfo) {
   const quizzes = document.querySelector(".allQuizzes");
   quizzes.innerHTML = "";
   for (let i = 0; i < quizzInfo.data.length; i++) {
     quizzes.innerHTML += `
-    <li id="${quizzInfo.data[i].id}" onclick="renderChoosenQuizz(this)">
+    <li id="${quizzInfo.data[i].id}" onclick="getChoosenQuizzData(this)">
       <img class="allQuizzes-img" src=${quizzInfo.data[i].image} alt="img do quizz">
       <p class="allQuizzes-title">${quizzInfo.data[i].title}</p>
     </li>
         `;
     }
-    globalQuizzInfo = quizzInfo.data;
-}  
-function renderChoosenQuizz(selection){
-    const selectionID= selection.getAttribute("id")
-    const quizzes = document.querySelector(".allQuizzes");
-    for (let i=0; i<globalQuizzInfo.length; i++){
-        if (selectionID == globalQuizzInfo[i].id)
-        {
-            quizzes.innerHTML=`
-            <li>
-            <img src="${globalQuizzInfo[i].image}">
-            <p>${globalQuizzInfo[i].title}</p>
-            </li>
-            `
-        }    
-    }
 };
+function getChoosenQuizzData(selection){
+    const selectionID= selection.getAttribute("id");
+    axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${selectionID}`)
+    .then(renderChoosenQuizz);
+};
+function renderChoosenQuizz(quizzInfo){
+  const quizzes = document.querySelector(".allQuizzes");
+  console.log(quizzInfo)
+  quizzes.innerHTML=`
+  <li>
+    <img src="${quizzInfo.data.image}">
+    <p>${quizzInfo.data.title}</p>
+  </li>
+  `
+}
 getQuizzes();

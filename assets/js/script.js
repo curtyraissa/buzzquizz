a0f5465b2eaa07afce4085d887bdc21a8063843a
SolForte <mateusbrandao1234@gmail.com>
2023-01-25 06:14:37 -0300

    Layout temporário para exibição do quiz para melhor compreensão de como está sendo inserido por innerHTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,15 +29,24 @@ function renderChoosenQuizz(quizzInfo){
   </div>
   `
   //Aqui embaixo é inserido cada questão individual do Quizz
-  //No caso, é uma div que tem o título da questão, e outra div que dentro dela contem as alternativas para esta questão
+  //No caso, é uma div que tem o título da questão, e outra div que dentro dela contem as alternativas para
+  //esta questão
   for (let i=0; i<quizzInfo.data.questions.length;i++){
+    //As classes dos elementos abaixo estão presentes aqui e no CSS somente para visualização melhor
+    //Desde que você olhe e entenda, pode fazer o que quiser, tipo mudar o nome da classe
+    //Eu recomendo criar novas classes nos elementos e, aliás se necessário, até mudar a função abaixo
+    //para poder fazer um layout com div melhor?
+    //desde que claro consiga entender o raciocínio
+    
+    //TL;DR: Pode fazer o que quiser com qualquer classe aqui abaixo exceto a "options-of-question-${i}"
     quizzes.innerHTML+=`
-    <div>
-      <p>${quizzInfo.data.questions[i].title}</p>
+    <div class="question-box">
+      <div class="question-declaration">
+        <p>${quizzInfo.data.questions[i].title}</p>
+      </div>
       <div class="options-of-question-${i}"></div>
     </div>`
-  //Atenção aqui em cima! Pode inserir classes adicionais para formatar como desejar no CSS,
-  //mas cuidado se for mexer nas classes já existentes
+
   }
   for (let i=0; i<quizzInfo.data.questions.length;i++){
     //Se não colocasse esse for aqui em cima, o query selector abaixo sempre escolheria a primeira

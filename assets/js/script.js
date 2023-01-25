a0ecac202324d98bc1c617792e40449c18870c09
CurtyRaissa <raissacurty@gmail.com>
2023-01-25 01:14:29 -0300

    add style function renderAllQuizzes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-function getQuizzes(){
-    const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-    promise.then(renderAllQuizzes);
+function getQuizzes() {
+  const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+  promise.then(renderAllQuizzes);
 }
-function renderAllQuizzes(quizzInfo){
-    const quizzes = document.querySelector(".allQuizzes");
-    quizzes.innerHTML = "";
-    for (let i=0; i< quizzInfo.data.length;i++){
-        quizzes.innerHTML += `
-        <li>
-        <img src=${quizzInfo.data[i].image} alt="">
-        <p>${quizzInfo.data[i].title}</p>
-        </li>
+function renderAllQuizzes(quizzInfo) {
+  const quizzes = document.querySelector(".allQuizzes");
+  quizzes.innerHTML = "";
+  for (let i = 0; i < quizzInfo.data.length; i++) {
+    quizzes.innerHTML += `
+    <li>
+      <img class="allQuizzes-img" src=${quizzInfo.data[i].image} alt="img do quizz">
+      <p class="allQuizzes-title">${quizzInfo.data[i].title}</p>
+    </li>
         `;
-    } 
+  }
 }
 getQuizzes();

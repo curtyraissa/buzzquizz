cb37d18781906a88e5e8924bce7e50c832720c13
SolForte <mateusbrandao1234@gmail.com>
2023-01-27 02:33:39 -0300

    Correção do gradiente

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,13 @@ function renderAllQuizzes(quizzInfo) {
   quizzes.innerHTML = "";
   for (let i = 0; i < quizzInfo.data.length; i++) {
     quizzes.innerHTML += `
-    <li id="${quizzInfo.data[i].id}" onclick="getChoosenQuizzData(this)">
-      <img class="allQuizzes-img" src=${quizzInfo.data[i].image} alt="img do quizz">
+
+    <li id="${quizzInfo.data[i].id}" onclick="getChoosenQuizzData(this)" class="allQuizzes-card" style="background-image: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizzInfo.data[i].image});">
       <p class="allQuizzes-title">${quizzInfo.data[i].title}</p>
     </li>
-        `;
+        `
+        ;
+//        <img class="allQuizzes-img" src=${quizzInfo.data[i].image} alt="img do quizz">
     }
   }
   };

d22623b79e3c6129083c77ca5d95fc313c9b8a21
SolForte <mateusbrandao1234@gmail.com>
2023-01-26 22:56:44 -0300

    Local storage functionalities sketch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 let currentQuizzData, currentMinValue, storedValue, storedValueLevel = undefined;
+let userId =[]
+let treatedUserId =[]
 function getQuizzes() {
   const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
   promise.then(renderAllQuizzes);
@@ -313,4 +315,28 @@ function informacoesNivelQuizz(){
   elemento4.forEach((valor)=> {valor.value = "";});
 }
 
+function getlocalStorage(){
+  const stringuserId = localStorage.getItem("id")
+  userId.push(stringuserId)
+  treatedUserId = JSON.parse("[" + userId + "]")
+}
+
+function criarQuizz(){
+    //A operação abaixo existe somente para testes através do envio direto do Quizz para a API
+    /*
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes",
+      {title:"T\xedtulo do quizz",image:"https://http.cat/411.jpg",questions:[{title:"T\xedtulo da pergunta 1",color:"#123456",answers:[{text:"Texto da resposta 1",image:"https://http.cat/411.jpg",isCorrectAnswer:!0},{text:"Texto da resposta 2",image:"https://http.cat/412.jpg",isCorrectAnswer:!1}]},{title:"T\xedtulo da pergunta 2",color:"#123456",answers:[{text:"Texto da resposta 1",image:"https://http.cat/411.jpg",isCorrectAnswer:!0},{text:"Texto da resposta 2",image:"https://http.cat/412.jpg",isCorrectAnswer:!1}]},{title:"T\xedtulo da pergunta 3",color:"#123456",answers:[{text:"Texto da resposta 1",image:"https://http.cat/411.jpg",isCorrectAnswer:!0},{text:"Texto da resposta 2",image:"https://http.cat/412.jpg",isCorrectAnswer:!1}]}],levels:[{title:"T\xedtulo do n\xedvel 1",image:"https://http.cat/411.jpg",text:"Descri\xe7\xe3o do n\xedvel 1",minValue:0},{title:"T\xedtulo do n\xedvel 2",image:"https://http.cat/412.jpg",text:"Descri\xe7\xe3o do n\xedvel 2",minValue:50}]
+    })
+    */
+    promise.then(criarQuizzPostProcessing)
+}
+function criarQuizzPostProcessing(variable){
+  const currentID=variable.data.id
+  userId.push(currentID)
+  localStorage.setItem("id",userId)
+  treatedUserId = JSON.parse("[" + userId + "]")
+}
+
+
+getlocalStorage(); //ESSA FUNÇÃO TEM DE SER SEMPRE A PRIMEIRA A SER EXECUTADA!
 getQuizzes();
